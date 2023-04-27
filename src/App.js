a3578f3c82d689dc9853c60809a0ec013906ea30
najwa tlili <tlilinajwa95@gmail.com>
2023-04-27 20:55:25 +0200

    JSX

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ProfileApp from './componnent/ProfilPhoto.js'
+import Adress from './componnent/Adress'
+import FullName from './componnent/FullName.js'
+
+import React from "react";
+const App = () => {
+  return (
+    <div> 
+    <ProfileApp/>
+    <Adress/>
+    <FullName/>
+    </div> )
+}
+export default App;
